5aa7f104d19de57d1b4e4c176dd2eedf6d815f23
kndrad <kon.now@protonmail.com>
2022-10-09 20:19:23 +0200

    new pkg base2 and base10.go file

--- main.go
+++ main.go
@@ -1,17 +1,7 @@
 package main
 
 import (
-	"base10"
-	"fmt"
 )
 
 func main() {
-	fmt.Println("Wynik:", base10.ToBase2(382))
-	fmt.Printf("\n\n")
-	fmt.Println("Wynik:", base10.ToBase2(2145))
-	fmt.Printf("\n\n")
-	fmt.Println("Wynik:", base10.ToBase2(0))
-	fmt.Printf("\n\n")
-	fmt.Println("Wynik:", base10.ToBase2(453))
-
 }
