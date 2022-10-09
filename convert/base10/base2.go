555c93cc0999d5b5e55f34f8537313ed32254944
kndrad <kon.now@protonmail.com>
2022-10-09 20:02:46 +0200

    removed unused imports

--- convert/base10/base2.go
+++ convert/base10/base2.go
@@ -5,6 +5,7 @@ import "fmt"
 // Does not calculate float numbers!
 // Returns number (eg. decimal) to binary number as a string.
 func ToBase2(num int) (b string) {
+	fmt.Printf("• %v\n", num)
 	factor := 2
 
 	digits := []int{}
@@ -15,20 +16,20 @@ func ToBase2(num int) (b string) {
 	quotient := num / factor
 
 	step := 1
-	fmt.Printf("%v) %v / %v = %v, remainder (reszta) %v\n", step, num, factor, quotient, remainder)
+	fmt.Printf("%v) %v / %v = %v, reszta %v\n", step, num, factor, quotient, remainder)
 
 	// loop breaks when quotient is 0
 	for {
+		step++
 		remainder = quotient % factor
 		digits = append([]int{remainder}, digits...) // append next remainder at the start of digits slice
 
 		quotient = quotient / factor
-		fmt.Printf("%v) %v / %v = %v, remainder (reszta) %v\n", step, num, factor, quotient, remainder)
-		
+		fmt.Printf("%v) %v / %v = %v, reszta %v\n", step, num, factor, quotient, remainder)
+
 		if quotient == 0 {
 			break
 		}
-		step++
 	}
 
 	for _, digit := range digits {
--- main.go
+++ main.go
@@ -3,14 +3,8 @@ package main
 import (
 	"base10"
 	"fmt"
-	"represent"
 )
 
-// Delete those later
-var _ = base10.ToBase2
-var _ = fmt.Print
-var _ = represent.InBase10
-
 func main() {
-	fmt.Println(base10.ToBase2(25867))
+	fmt.Println("Wynik:", base10.ToBase2(453))
 }
